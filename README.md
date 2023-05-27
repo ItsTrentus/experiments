043405ad0b68305404c328636645d5133290d3ef
Trentus <trent@iamtrent.us>
2023-05-27 08:28:55 -0400

    expirments setup

---
+++ README.md
@@ -0,0 +1,1 @@
+A running list of random experiments that I'll work onto test out a concept.
---
+++ react-redux/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <h1 className="title">ToDo App Generic, Yay</h1>
+                <ul role="list">
+                    <li className="list-item">
+                        <input type="checkbox" />
+                        <span>Item 1</span>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default App;
